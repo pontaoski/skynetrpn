8b6aa6484592d714996adc3b4aef478517eec4c4
Janet Blackquill <uhhadd@gmail.com>
2021-07-04 23:57:34 -0400

    make it turing complete

--- main.go
+++ main.go
@@ -29,113 +29,190 @@ func (s *Stack) Top() interface{} {
 	return s.data[len(s.data)-1]
 }
 
-func main() {
-	// make a reader that reads words from stdin
-	b := bufio.NewScanner(os.Stdin)
-	b.Split(bufio.ScanWords)
+type state struct {
+	stack     *Stack
+	input     *bufio.Scanner
+	functions map[string]func(*state)
+}
 
-	s := new(Stack)
+func newState() *state {
+	s := &state{
+		stack: &Stack{},
+		input: bufio.NewScanner(os.Stdin),
+		functions: map[string]func(*state){
+			"+": func(s *state) {
+				a := s.stack.Pop().(int)
+				b := s.stack.Pop().(int)
+				s.stack.Push(a + b)
+			},
+			"-": func(s *state) {
+				a := s.stack.Pop().(int)
+				b := s.stack.Pop().(int)
+				s.stack.Push(b - a)
+			},
+			"*": func(s *state) {
+				a := s.stack.Pop().(int)
+				b := s.stack.Pop().(int)
+				s.stack.Push(a * b)
+			},
+			"/": func(s *state) {
+				a := s.stack.Pop().(int)
+				b := s.stack.Pop().(int)
+				s.stack.Push(b / a)
+			},
+			"%": func(s *state) {
+				a := s.stack.Pop().(int)
+				b := s.stack.Pop().(int)
+				s.stack.Push(b % a)
+			},
+			"^": func(s *state) {
+				a := s.stack.Pop().(int)
+				b := s.stack.Pop().(int)
+				s.stack.Push(b ^ a)
+			},
+			"dup": func(s *state) {
+				s.stack.Push(s.stack.Top())
+			},
+			"pop": func(s *state) {
+				s.stack.Pop()
+			},
+			"swap": func(s *state) {
+				a := s.stack.Pop().(int)
+				b := s.stack.Pop().(int)
+				s.stack.Push(a)
+				s.stack.Push(b)
+			},
+			".": func(s *state) {
+				fmt.Println(s.stack.Pop())
+			},
+			"sigil:#": func(s *state) {
+				word := s.stack.Pop().(string)
+				// if the received word is a number, push it to the stack
+				if num, err := strconv.Atoi(word); err == nil {
+					s.stack.Push(num)
+				} else {
+					panic("bad number")
+				}
+			},
+			"sigil:'": func(s *state) {
+				word := s.stack.Pop().(string)
+				s.stack.Push(word)
+			},
+			"sigil:&": func(s *state) {
+				word := s.stack.Pop().(string)
+				if fn, ok := s.functions[word]; ok {
+					s.stack.Push(fn)
+				} else {
+					panic("unknown word: " + word)
+				}
+			},
+			"call": func(s *state) {
+				fn := s.stack.Pop().(func(*state))
+				fn(s)
+			},
+			"def": func(s *state) {
+				fn := s.stack.Pop().(func(*state))
+				word := s.stack.Pop().(string)
+				s.functions[word] = fn
+			},
+			"true": func(s *state) {
+				s.stack.Push(true)
+			},
+			"false": func(s *state) {
+				s.stack.Push(false)
+			},
+			"if": func(s *state) {
+				fn := s.stack.Pop().(func(*state))
+				cond := s.stack.Pop().(bool)
+				if cond {
+					fn(s)
+				}
+			},
+			"while": func(s *state) {
+				body := s.stack.Pop().(func(*state))
+				cond := s.stack.Pop().(func(*state))
 
-	// map of names to functions
-	functions := map[string]func(*Stack){
-		"+": func(s *Stack) {
-			a := s.Pop().(int)
-			b := s.Pop().(int)
-			s.Push(a + b)
-		},
-		"-": func(s *Stack) {
-			a := s.Pop().(int)
-			b := s.Pop().(int)
-			s.Push(b - a)
-		},
-		"*": func(s *Stack) {
-			a := s.Pop().(int)
-			b := s.Pop().(int)
-			s.Push(a * b)
-		},
-		"/": func(s *Stack) {
-			a := s.Pop().(int)
-			b := s.Pop().(int)
-			s.Push(b / a)
-		},
-		"%": func(s *Stack) {
-			a := s.Pop().(int)
-			b := s.Pop().(int)
-			s.Push(b % a)
-		},
-		"^": func(s *Stack) {
-			a := s.Pop().(int)
-			b := s.Pop().(int)
-			s.Push(b ^ a)
-		},
-		"dup": func(s *Stack) {
-			s.Push(s.Top())
-		},
-		"pop": func(s *Stack) {
-			s.Pop()
-		},
-		"swap": func(s *Stack) {
-			a := s.Pop().(int)
-			b := s.Pop().(int)
-			s.Push(a)
-			s.Push(b)
-		},
-		".": func(s *Stack) {
-			fmt.Println(s.Pop())
+				cond(s)
+				for s.stack.Pop().(bool) {
+					body(s)
+					cond(s)
+				}
+			},
+			">": func(s *state) {
+				a := s.stack.Pop().(int)
+				b := s.stack.Pop().(int)
+				s.stack.Push(b > a)
+			},
+			"<": func(s *state) {
+				a := s.stack.Pop().(int)
+				b := s.stack.Pop().(int)
+				s.stack.Push(b < a)
+			},
+			"dbg": func(s *state) {
+				fmt.Println(s.stack)
+			},
 		},
 	}
+	s.input.Split(bufio.ScanWords)
+	return s
+}
 
-	// read a stream of words from the scanner
-	for b.Scan() {
-		// get the word from the scanner
-		word := b.Text()
-
-		// if the word is in our functions dict, call the function
-		if f, ok := functions[word]; ok {
+func (s *state) handleWord(w string) func() {
+	f, ok := s.functions["sigil:"+string(w[0])]
+	if ok {
+		return func() {
+			s.stack.Push(w[1:])
 			f(s)
-			continue
-		}
-
-		// if the received word is a number, push it to the stack
-		if num, err := strconv.Atoi(word); err == nil {
-			s.Push(num)
-			continue
-		}
-
-		// if the word starts with a colon, begin reading words until a semicolon is found,
-		// at which it is compiled into a function inserted into the functions dict
-		if word[0] == ':' {
-			// get the function name
-			name := word[1:]
-			// get the function body
-			words := []string{}
-			for b.Scan() {
-				if b.Text() == ";" {
-					break
-				}
-				words = append(words, b.Text())
-			}
-			func(wrds []string) {
-				// compile the function
-				functions[name] = func(s *Stack) {
-					for _, wrd := range wrds {
-						// if the word is in our functions dict, call the function
-						if f, ok := functions[wrd]; ok {
-							f(s)
-							continue
-						}
-
-						// if the received word is a number, push it to the stack
-						if num, err := strconv.Atoi(wrd); err == nil {
-							s.Push(num)
-							continue
-						}
-
-						panic("bad word" + wrd)
-					}
-				}
-			}(words)
-			continue
 		}
 	}
+	f, ok = s.functions[w]
+	if !ok {
+		panic("unknown word: " + w)
+	}
+	return func() {
+		f(s)
+	}
+}
+
+func (s *state) parseLambda() func(s *state) {
+	fns := []func(*state){}
+	for s.input.Scan() && s.input.Text() != "]" {
+		w := s.input.Text()
+		f, ok := s.functions["sigil:"+string(w[0])]
+		if ok {
+			fns = append(fns, func(s *state) {
+				s.stack.Push(w[1:])
+				f(s)
+			})
+			continue
+		}
+		f, ok = s.functions[w]
+		if !ok {
+			panic("unknown word: " + w)
+		}
+		fns = append(fns, f)
+	}
+	return func(s *state) {
+		for _, f := range fns {
+			f(s)
+		}
+	}
+}
+
+func (s *state) run() {
+	for s.input.Scan() {
+		word := s.input.Text()
+		switch word {
+		case "[":
+			s.stack.Push(s.parseLambda())
+		default:
+			f := s.handleWord(word)
+			f()
+		}
+	}
+}
+
+func main() {
+	state := newState()
+	state.run()
 }
